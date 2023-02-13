54d4a1b2ff805a55229df1fa816a21fd2aa1c34b
THIRUKKURAL SELVAN <akauriht@gmail.com>
2023-02-13 21:27:12 +0530

    Attempted to add mini prohjects

--- scripts.js
+++ scripts.js
@@ -2,18 +2,30 @@
 var tabLinks = document.getElementsByClassName('tab-links');
 var tabContents = document.getElementsByClassName('tab-contents');
 
+
+
 function openTab(tabName) {
-    
-    for(let tabLink of tabLinks) {
-        tabLink.classList.remove("active-link");
+
+    for (let tabLink of tabLinks) {
+        tabLink
+            .classList
+            .remove("active-link");
     }
 
-    for(let tabContent of tabContents) {
-        tabContent.classList.remove("active-tab");
+    for (let tabContent of tabContents) {
+        tabContent
+            .classList
+            .remove("active-tab");
     }
 
-    event.currentTarget.classList.add('active-link');
-    document.getElementById(tabName).classList.add('active-tab');
+    event
+        .currentTarget
+        .classList
+        .add('active-link');
+    document
+        .getElementById(tabName)
+        .classList
+        .add('active-tab');
 }
 
 // ---------------- JS for Mobile Menu ------------------
