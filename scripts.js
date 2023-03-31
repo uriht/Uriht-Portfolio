c7655af02adb7d07f5f2e9cefe84b74c2631be4d
THIRUKKURAL SELVAN <akauriht@gmail.com>
2023-04-01 00:27:07 +0530

    Added Resume for Download

--- scripts.js
+++ scripts.js
@@ -37,3 +37,28 @@ function openMenu() {
 function closeMenu() {
     mobileMenu.style.right = "-400px";
 }
+
+// This is Resume text JS
+const animateTextTyping = (node) => {
+	const text = node.textContent;
+	const chars = text.split("");
+	node.innerHTML = "";
+	node.classList.add("typing");
+	i = 0;
+	const addNextChar = (i) => {
+		let nextChar = chars[i];
+		node.innerHTML += "<span>" + nextChar + "<span>";
+		if (i < chars.length - 1) {
+			setTimeout(function () {
+				addNextChar(i + 1);
+			}, 50 + Math.random() * 100); // add random delay to make it more human ;)
+		} else {
+			setTimeout(function () {
+				node.classList.remove("typing");
+			}, 50 + Math.random() * 150);
+		}
+	};
+	addNextChar(i);
+};
+let text = document.getElementById("text");
+animateTextTyping(text);
